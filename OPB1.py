a894d8066b22f712f3ed5fb12b426ef31c27f856
iszacj <133179592+iszacj@users.noreply.github.com>
2023-05-11 14:01:14 +1200

    Update OPB1.py

--- OPB1.py
+++ OPB1.py
@@ -3,15 +3,15 @@ from discord.ext import commands
 import random
 
 intents = discord.Intents.default()
-intents.typing = True
-intents.presences = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-
 spawned_character = None  # Variable to store the currently spawned character
-# temporary_characters = []  # Temporary list of characters
+temporary_characters = ["Luffy", "Zoro", "Nami", "Usopp", "Sanji", "Chopper"]  # Temporary list of characters
+
+message_counter = 0
+target_message_count = random.randint(5, 10)
 
 @bot.event
 async def on_ready():
@@ -22,13 +22,22 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    # Message tracking code
-    for guild in bot.guilds:
-        for channel in guild.channels:
-            if isinstance(channel, discord.TextChannel):
-                # Track messages in text channels only
-                # Increment the message counter
-                pass
+    if message.content.startswith('!') or message.content.startswith('?'):  # Exclude bot commands
+        return
+
+    global spawned_character
+    global message_counter
+    global target_message_count
+
+    message_counter += 1
+
+    if message_counter >= target_message_count:
+        if spawned_character is not None:
+            await message.channel.send(f"The character {spawned_character} sailed away!")
+            spawned_character = None
+
+        message_counter = 0
+        target_message_count = random.randint(5, 10)
 
     await bot.process_commands(message)
 
@@ -37,13 +46,11 @@ async def spawn(ctx):
     global spawned_character
 
     if spawned_character is None:
-        characters = temporary_characters if temporary_characters else load_character_list()
-
-        if not characters:
+        if not temporary_characters:
             await ctx.send("No characters available.")
             return
         
-        spawned_character = random.choice(characters)
+        spawned_character = random.choice(temporary_characters)
         # Code to send the image of the spawned character to the Discord channel
         await ctx.send(f"A One Piece character has been spawned! Guess the name using the command `!guess [name]`.")
         await ctx.send("Image of the character")  # Replace with code to send the character's image
@@ -75,22 +82,4 @@ async def guess(ctx, character_name):
     else:
         await ctx.send("No character has been spawned yet. Use the `!spawn` command to spawn a character.")
 
-def load_character_list():
-    with open('characters.txt', 'r') as file:
-        return file.read().splitlines()
-
-# Temporary list of characters
-temporary_characters = [
-    'Luffy',
-    'Zoro',
-    'Nami',
-    'Usopp',
-    'Sanji',
-    'Chopper',
-    'Robin',
-    'Franky',
-    'Brook',
-    'Jinbe'
-]
-
-bot.run('MTEwNjAxMzA1NTQ5OTg5NDg5Ng.GUA5zD.iEkLngUcfwSDto1zJtcPQrVM-o9AAJ7ZeTMj3M')
+bot.run('')
