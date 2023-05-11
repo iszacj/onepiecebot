e1f20135a93c5672488a7b961af814c85fbd5ff8
iszacj <133179592+iszacj@users.noreply.github.com>
2023-05-11 18:28:52 +1200

    b

--- OPB1.py
+++ OPB1.py
@@ -8,10 +8,27 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 spawned_character = None  # Variable to store the currently spawned character
-temporary_characters = ["Luffy", "Zoro", "Nami", "Usopp", "Sanji", "Chopper"]  # Temporary list of characters
+temporary_characters = {
+    "Luffy": "https://cdn.discordapp.com/attachments/1106078732457873459/1106078788984520805/luffy.png",
+    "Zoro": "https://cdn.discordapp.com/attachments/1106078732457873459/1106078924347297812/zoro.png",
+    "Nami": "https://cdn.discordapp.com/attachments/1106078732457873459/1106079299460669480/nami.png",
+    "Usopp": "https://cdn.discordapp.com/attachments/1106078732457873459/1106078924015935558/usopp.png",
+    "Sanji": "https://cdn.discordapp.com/attachments/1106078732457873459/1106078923613286440/sanji.png",
+    "Chopper": "https://cdn.discordapp.com/attachments/1106078732457873459/1106078922795393035/chopper.png"
+}  # Temporary dictionary of characters and their corresponding image URLs
+
+import random
+
+def generate_hint(character_name):
+    name_list = list(character_name)
+    hint_length = random.randint(1, len(character_name) // 2)  # Random length for the hint
+    hint_indices = random.sample(range(len(character_name)), hint_length)  # Random indices for the hint letters
+    hint = [name_list[i] if i in hint_indices else '_' for i in range(len(character_name))]
+    formatted_hint = ' '.join(hint)  # Separate each character with a space for better visibility
+    return f'`{formatted_hint}`'
 
 message_counter = 0
-target_message_count = random.randint(5, 10)
+target_message_count = 0
 
 @bot.event
 async def on_ready():
@@ -22,12 +39,12 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    if message.content.startswith('!') or message.content.startswith('?'):  # Exclude bot commands
-        return
-
     global spawned_character
     global message_counter
     global target_message_count
+
+    if target_message_count == 0:
+        target_message_count = random.randint(10, 20)
 
     message_counter += 1
 
@@ -37,11 +54,11 @@ async def on_message(message):
             spawned_character = None
 
         message_counter = 0
-        target_message_count = random.randint(5, 10)
+        target_message_count = random.randint(10, 20)
 
     await bot.process_commands(message)
 
-@bot.command()
+@bot.command(aliases=['s'])
 async def spawn(ctx):
     global spawned_character
 
@@ -50,36 +67,79 @@ async def spawn(ctx):
             await ctx.send("No characters available.")
             return
         
-        spawned_character = random.choice(temporary_characters)
-        # Code to send the image of the spawned character to the Discord channel
-        await ctx.send(f"A One Piece character has been spawned! Guess the name using the command `!guess [name]`.")
-        await ctx.send("Image of the character")  # Replace with code to send the character's image
+        spawned_character = random.choice(list(temporary_characters.keys()))
+        character_image_url = temporary_characters[spawned_character]
+        
+        await ctx.send(f"A One Piece character has been spawned! Catch the character using the command `!catch [name]`.")
+        await ctx.send(character_image_url)  # Send the character's image URL
     else:
-        await ctx.send("A character has already been spawned. Guess the current character or wait for the next one.")
+        await ctx.send("A character has already been spawned. Catch the current character or wait for the next one.")
 
-@bot.command()
+@bot.command(aliases=['h'])
 async def hint(ctx):
     global spawned_character
 
     if spawned_character is not None:
-        # Code to provide a hint by revealing 2-3 random letters of the character's name
-        await ctx.send("Here's a hint: [Hint]")  # Replace [Hint] with the actual hint
+        hint = generate_hint(spawned_character)
+        await ctx.send(f"Here's a hint: {hint}")
     else:
-        await ctx.send("No character has been spawned yet. Use the `!spawn` command to spawn a character.")
+        await ctx.send("No character has been spawned yet.")
 
-@bot.command()
-async def guess(ctx, character_name):
+user_scores = {}  # Dictionary to store user scores
+
+user_crews = {}  # Dictionary to store user crews
+
+user_crews = {}  # Dictionary to store user crews
+
+@bot.command(aliases=['c'])
+async def catch(ctx, character_name):
     global spawned_character
 
     if spawned_character is not None:
         if character_name.lower() == spawned_character.lower():
-            # Code to handle correct guess
-            await ctx.send(f"Congratulations! You caught {spawned_character}.")  # Customize the success message
+            # Code to handle correct catch
+            user = ctx.author
+            if user.id not in user_crews:
+                user_crews[user.id] = set()
+            user_crews[user.id].add(spawned_character)
+            await ctx.send(f"Congratulations! You caught {spawned_character}. {spawned_character} has joined your pirate crew.")
             spawned_character = None  # Reset spawned character
         else:
-            # Code to handle incorrect guess
-            await ctx.send("Incorrect guess. Try again or use the `!hint` command for a hint.")
+            # Code to handle incorrect catch
+            await ctx.send("Incorrect catch. Try again or use the `!hint` command for a hint.")
     else:
         await ctx.send("No character has been spawned yet. Use the `!spawn` command to spawn a character.")
 
-bot.run('')
+@bot.command(aliases=['pc', 'crew'])
+async def pirate_crew(ctx):
+    user = ctx.author
+    if user.id in user_crews:
+        crew = sorted(user_crews[user.id])
+        if crew:
+            crew_list = "\n".join(crew)
+            response = f"```\nYour Pirate Crew:\n\n{crew_list}\n```"
+
+            # Adjust the width of the response
+            lines = response.split("\n")
+            max_width = max(len(line) for line in lines)  # Find the maximum line width
+            response = "```\n"
+            response += "Your Pirate Crew:".ljust(max_width // 3 + 3) + "\n\n"  # Adjust the width of the title
+            response += "\n".join(line.ljust(max_width // 3) for line in lines[3:])  # Adjust the width of the crew list
+            response += "\n```"
+
+            await ctx.send(response)
+        else:
+            await ctx.send("Your pirate crew is empty. Catch some characters using the `!catch` command!")
+    else:
+        await ctx.send("Your pirate crew is empty. Catch some characters using the `!catch` command!")
+
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        return  # Ignore command not found errors
+
+    await ctx.send(f"An error occurred: {str(error)}")
+
+bot.run('MTEwNjAxMzA1NTQ5OTg5NDg5Ng.GiypfL.QMKBn0NIv6qROBgWWFhmjucEOrjz4WaKXghJBo')
